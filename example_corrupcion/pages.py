c5949682cc05504ee357c952cb4bac2ea2315961
unknown <FCSH@GGCSHLABWRK426.espol.edu.ec>
2018-05-09 17:28:36 -0500

    experimento corruccion 75% terminado

--- example_corrupcion/pages.py
+++ example_corrupcion/pages.py
@@ -43,7 +43,7 @@ class WaitForP1(WaitPage):
     pass
 
 class ResultsWaitPage(WaitPage):
-    
+    form_model = 'group'
 
     def after_all_players_arrive(self):
         group = self.group
@@ -52,14 +52,80 @@ class ResultsWaitPage(WaitPage):
         if(group.aceptarCoima == 1):
             p1.payoff = (Constants.tokens1-2)- group.coinsJ1
             p2.payoff = Constants.tokens2 + ((group.coinsJ1)*3)
+            if(group.coinsJ1 == 1):
+                if(group.opcionCoima == 'A'):
+                    p1.payoff = 47
+                    p2.payoff = 63
+                else:
+                    p1.payoff = 67
+                    p2.payoff = 48
+            if(group.coinsJ1 == 2):
+                if(group.opcionCoima == 'A'):
+                    p1.payoff = 45
+                    p2.payoff = 56
+                else:
+                    p1.payoff = 66
+                    p2.payoff = 51
+            if(group.coinsJ1 == 3):
+                if(group.opcionCoima == 'A'):
+                    p1.payoff = 45
+                    p2.payoff = 59
+                else:
+                    p1.payoff = 65
+                    p2.payoff = 54
+            if(group.coinsJ1 == 4):
+                if(group.opcionCoima == 'A'):
+                    p1.payoff = 44
+                    p2.payoff = 62
+                else:
+                    p1.payoff = 64
+                    p2.payoff = 57
+            if(group.coinsJ1 == 5):
+                if(group.opcionCoima == 'A'):
+                    p1.payoff = 43
+                    p2.payoff = 65
+                else:
+                    p1.payoff = 63
+                    p2.payoff = 60
+            if(group.coinsJ1 == 6):
+                if(group.opcionCoima == 'A'):
+                    p1.payoff = 42
+                    p2.payoff = 68
+                else:
+                    p1.payoff = 62
+                    p2.payoff = 63
+            if(group.coinsJ1 == 7):
+                if(group.opcionCoima == 'A'):
+                    p1.payoff = 41
+                    p2.payoff = 71
+                else:
+                    p1.payoff = 61
+                    p2.payoff = 66
+            if(group.coinsJ1 == 8):
+                if(group.opcionCoima == 'A'):
+                    p1.payoff = 40
+                    p2.payoff = 74
+                else:
+                    p1.payoff = 60
+                    p2.payoff = 69
+            if(group.coinsJ1 == 9):
+                if(group.opcionCoima == 'A'):
+                    p1.payoff = 39
+                    p2.payoff = 77
+                else:
+                    p1.payoff = 59
+                    p2.payoff = 72
+            if(group.coinsJ1 == 10):
+                if(group.opcionCoima == 'A'):
+                    p1.payoff = 38
+                    p2.payoff = 80
+                else:
+                    p1.payoff = 58
+                    p2.payoff = 75
         else:
             p1.payoff = (Constants.tokens1-2)- group.coinsJ1
             p2.payoff = Constants.tokens2 
         
-            
-
-
-
 
 class Results(Page):
     pass
