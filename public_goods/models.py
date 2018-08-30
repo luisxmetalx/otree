9697dc0730aa98f7f9a80c7e16d91778a6dae5f2
luisxmetalx <junitho_93@hotmail.com>
2018-08-30 11:39:18 -0500

    public goods

---
+++ public_goods/models.py
@@ -0,0 +1,72 @@
+from otree.api import (
+    models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
+    Currency as c, currency_range
+)
+
+
+author = 'Your name here'
+
+doc = """
+Your app description
+"""
+
+
+class Constants(BaseConstants):
+    name_in_url = 'public_goods'
+    players_per_group = 3
+    num_rounds = 1
+
+    instructions_template = 'public_goods/Instructions.html'
+
+    # """Amount allocated to each player"""
+    endowment = 100
+    multiplier = 2
+
+
+class Subsession(BaseSubsession):
+    def vars_for_admin_report(self):
+        contributions = [p.contribution for p in self.get_players() if p.contribution != None]
+        if contributions:
+            return {
+                'avg_contribution': sum(contributions)/len(contributions),
+                'min_contribution': min(contributions),
+                'max_contribution': max(contributions),
+            }
+        else:
+            return {
+                'avg_contribution': '(no data)',
+                'min_contribution': '(no data)',
+                'max_contribution': '(no data)',
+            }
+
+
+class Group(BaseGroup):
+    total_contribution = models.CurrencyField()
+
+    individual_share = models.CurrencyField()
+
+    def set_payoffs(self):
+        self.total_contribution = sum([p.contribution for p in self.get_players()])
+        self.individual_share = self.total_contribution * Constants.multiplier / Constants.players_per_group
+        for p in self.get_players():
+            p.payoff = (Constants.endowment - p.contribution) + self.individual_share
+
+
+class Player(BasePlayer):
+    #genero de los participante
+    genero = models.StringField(
+        choices=['Masculino', 'Femenino'],
+        doc="""Esto es el género del jugador""",
+        widget=widgets.RadioSelect
+    )
+
+    #edad del participante
+    edad = models.IntegerField(min=18)
+    
+    #matricula del jugador
+    matricula = models.IntegerField(min=100000000,max=999999999)
+
+    contribution = models.CurrencyField(
+        min=0, max=Constants.endowment,
+        doc="""The amount contributed by the player""",
+    )
