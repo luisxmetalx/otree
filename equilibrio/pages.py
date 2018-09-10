4201f2842c7552ddbc0f80fbcaf1c00448422fad
unknown <FCSH@GGCSHLABWRK426.espol.edu.ec>
2018-09-10 16:29:19 -0500

    seguir con el experimento

--- equilibrio/pages.py
+++ equilibrio/pages.py
@@ -8,6 +8,10 @@ class Definir_precio(Page):
     form_model = models.Player
     form_fields = ['precio']
 
+    def precio_error_message(self, value):
+        if value < self.player.carta:
+            return 'valor no permitido...'
+
     def is_displayed(self):
 
         return True
@@ -17,6 +21,8 @@ class ResultsWaitPage(WaitPage):
     def after_all_players_arrive(self):
         pass
 
+class Negociacion(Page):
+    pass
 
 class Results(Page):
     pass
@@ -25,5 +31,6 @@ class Results(Page):
 page_sequence = [
     Definir_precio,
     ResultsWaitPage,
+    Negociacion,
     Results
 ]
